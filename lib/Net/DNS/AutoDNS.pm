56bab8e3452792371407c936ce9b027c000f195b
Tobias Kirschstein <lev@ideegeo.com>
2013-07-07 21:49:24 +0200

    initial commit

---
+++ lib/Net/DNS/AutoDNS.pm
@@ -0,0 +1,92 @@
+package Net::DNS::AutoDNS;
+
+use 5.012;
+use Mouse;
+
+# ABSTRACT: 
+
+# VERSION
+
+use Data::Dump;
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Some::Module;
+    use Data::Dump 'dump';
+
+=head1 ATTRIBUTES
+
+=head2 attr
+
+=cut
+
+has 'attr' => (
+    is => 'rw',
+);
+
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method
+
+=cut
+
+sub method {
+    my ($self) = @_;
+}
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests on GitHub's issue tracker L<https://github.com/<github_user>/Net::DNS::AutoDNS/issues>.
+Pull requests welcome.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::DNS::AutoDNS
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GitHub repository
+
+L<https://github.com/<github_user>/Net::DNS::AutoDNS>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/module/Net::DNS::AutoDNS>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net::DNS::AutoDNS>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net::DNS::AutoDNS>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=over 4
+
+=item * Lenz Gschwendtner (@norbu09), for being an awesome mentor and friend.
+
+=back
+
+=cut
+
+1;  # End of Net::DNS::AutoDNS
